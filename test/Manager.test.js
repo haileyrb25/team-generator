89783fd2510cdecbfdab247e9e38504a06c77ac4
Hailey Bates <hailey.bates001@gmail.com>
2022-04-26 22:43:35 -0400

    Added big constructor to employee file

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,4 +1,12 @@
 //require manager and employee from lib
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
+
 //can set office number
+describe("Set office number via constructor argument", ()=> {
+    const testNum = 100;
+    const newHire = new Manager("Sam", 1, "sam@gmail.com", testNum);
+    expect(newHire.officeNumber).toBe(testNum);
+})
 //can get office number
 //role returns manager
