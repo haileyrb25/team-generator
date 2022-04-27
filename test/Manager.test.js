4218bfb457f9f064232bf06e942456e62eb2c344
Hailey Bates <hailey.bates001@gmail.com>
2022-04-26 22:45:40 -0400

    Changed const for office number

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,9 +4,9 @@ const Employee = require("../lib/Employee");
 
 //can set office number
 describe("Set office number via constructor argument", ()=> {
-    const testNum = 100;
-    const newHire = new Manager("Sam", 1, "sam@gmail.com", testNum);
-    expect(newHire.officeNumber).toBe(testNum);
+    const officeNum = 100;
+    const newHire = new Manager("Sam", 1, "sam@gmail.com", officeNum);
+    expect(newHire.officeNumber).toBe(officeNum);
 })
 //can get office number
 //role returns manager
