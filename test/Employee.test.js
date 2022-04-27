ff6ee1b004df738ed47be32d5503cd79a63319de
Hailey Bates <hailey.bates001@gmail.com>
2022-04-26 22:13:26 -0400

    Added all files needed for the challenge

---
+++ test/Employee.test.js
@@ -0,0 +1,33 @@
+// require the employee from the lib folder
+const Employee = require("../lib/Employee"); 
+
+
+// test can begin employee instance
+
+
+
+//test can set name via constructor argument
+
+
+
+//test can set id via constructor argument
+
+
+
+//test can set email via constructor argument
+
+
+
+//test can set name via constructor function
+
+
+
+//test can set id via constructor function
+
+
+
+//test can set email via constructor function
+
+
+
+//role function should return employee
